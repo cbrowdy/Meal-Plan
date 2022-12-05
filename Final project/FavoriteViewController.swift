a00b131dbce3e1e690ba0a1b04aed7e96a5fe9c3
Ian Youth <connorbrowder@wustl.edu>
2022-12-05 03:24:12 -0600

    connor update

---
+++ Final project/FavoriteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteViewController.swift
+//  Final project
+//
+//  Created by Sproull Student on 12/5/22.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
