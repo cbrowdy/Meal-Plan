9080e998d9710c7839d2a3530315646d2396f190
omermv <112492724+omermv@users.noreply.github.com>
2022-12-05 04:30:06 -0600

    ok

--- Final project/Final project/FavoriteViewController.swift
+++ Final project/Final project/FavoriteViewController.swift
@@ -15,7 +15,11 @@ class FavoriteViewController: UIViewController, UITableViewDataSource, UITabBarC
     
     @IBOutlet weak var favoritesTableView: UITableView!
     
-    var foodID : Int!
+    var favoriteRecipesData: [[AnyHashable:Any]] = []
+    var recipeID : Int! = 664474
+//    var recipeTitle: String!
+//    var recipeImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.delegate = self
@@ -24,22 +28,33 @@ class FavoriteViewController: UIViewController, UITableViewDataSource, UITabBarC
                 guard let value = snapshot.value as? [String: Any] else {return}
                 guard let userName = value["User"] as? String else {return}
                 guard let fID = value["ID"] as? Int else {return}
-                self.foodID = fID
+                self.recipeID = fID
                 if(userName == Auth.auth().currentUser!.uid) {
                     print(value)
-                    print(String(self.foodID))
+                    print(String(self.recipeID))
                 }
             })
         }
-        
+        LoadRecipeData()
         setupTableView()
         // Do any additional setup after loading the view.
     }
     
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        if tabBarController.selectedIndex == 1{
-            favoritesTableView.reloadData()
-        }
+    func LoadRecipeData(){
+        let randomRecipeURL = URL(string: "https://api.spoonacular.com/recipes/\(recipeID!)/information?apiKey=1fb7da19f9b4445aa293b61cfa6d479f&includeNutrition=false")
+               let binaryRandomRecipeResults = try! Data(contentsOf: randomRecipeURL!)
+               if let currentRecipeResults = try! JSONSerialization.jsonObject(with: binaryRandomRecipeResults, options: .fragmentsAllowed) as? [AnyHashable: Any]{
+                   favoriteRecipesData.append(currentRecipeResults)
+//                   recipeTitle = recipeData["title"] as? String ?? ""
+//                   if let stringRecipeImageURL = recipeData["image"] as? String {
+//                       let recipeImageURL = URL(string: stringRecipeImageURL)
+//                       let data = try? Data(contentsOf: recipeImageURL!)
+//                       recipeImage = UIImage(data: data!)
+//                   }
+//                   else {
+//                       recipeImage = UIImage(named: "No-Image-Placeholder.svg")
+//                   }
+               }
     }
     
     func setupTableView() {
@@ -48,29 +63,44 @@ class FavoriteViewController: UIViewController, UITableViewDataSource, UITabBarC
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
-        return favoritesMoviesID.count
+//        let favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
+//        return favoritesMoviesID.count
+        return 1;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        let favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
-        cell.textLabel!.text = UserDefaults.standard.string(forKey: String(favoritesMoviesID[indexPath.row]))
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+//        let favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
+//        cell.textLabel!.text = UserDefaults.standard.string(forKey: String(favoritesMoviesID[indexPath.row]))
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel!.text = favoriteRecipesData[indexPath.row]["title"] as? String
+        if let stringRecipeImageURL = favoriteRecipesData[indexPath.row]["image"] as? String {
+                       let recipeImageURL = URL(string: stringRecipeImageURL)
+                       let data = try? Data(contentsOf: recipeImageURL!)
+            cell.imageView?.image = UIImage(data: data!)
+                   }
+                   else {
+                       cell.imageView?.image = UIImage(named: "No-Image-Placeholder.svg")
+                   }
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            var favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
-            let movieIDToDelete: Int = favoritesMoviesID[indexPath.row]
-            favoritesMoviesID = favoritesMoviesID.filter{$0 != movieIDToDelete}
-            UserDefaults.standard.removeObject(forKey: String(movieIDToDelete))
-            UserDefaults.standard.set(favoritesMoviesID, forKey: "favoritesMoviesID")
+//            var favoritesMoviesID: [Int] = UserDefaults.standard.array(forKey: "favoritesMoviesID") as! [Int]
+//            let movieIDToDelete: Int = favoritesMoviesID[indexPath.row]
+//            favoritesMoviesID = favoritesMoviesID.filter{$0 != movieIDToDelete}
+//            UserDefaults.standard.removeObject(forKey: String(movieIDToDelete))
+//            UserDefaults.standard.set(favoritesMoviesID, forKey: "favoritesMoviesID")
             favoritesTableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
-
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if tabBarController.selectedIndex == 1{
+            favoritesTableView.reloadData()
+        }
+    }
     /*
     // MARK: - Navigation
 
